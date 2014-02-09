6acd59d389917da0cf49421e25ccb44db54a42ae
Maximilian Marcoll <quince@maximilianmarcoll.de>
2014-02-09 13:37:55 +0100

    PanelHacking - Made sure we can still add categories

--- panel/snippets/pages.add.php
+++ panel/snippets/pages.add.php
@@ -29,6 +29,13 @@ $templates = data::findTemplates();
         <input type="text" name="uid" value="<?php echo html(get('uid')) ?>" />
       </div>
 
+                         <!-- manually prevented the popup from being shown when the page is of type 'project'.
+                        the population of the menu is then limited to "media type" - templates
+                        in the end i made sure that the default-template is chosen if the panel is "home"
+                        so that we can add categories which are then NOT of type 'project' but use the
+                        default template -->
+
+                        
       <div class="field">
         <?php if(count($templates) == 1): ?>
           <?php $template = a::first($templates) ?>
@@ -51,7 +58,10 @@ $templates = data::findTemplates();
             </select>
           
           <?php else:
-              $template = get('template', 'project')
+              if($panel->isHome)
+                $template = get('template', 'default');
+              else
+                $template = get('template', 'project');
           ?>
           
           <input type="hidden" name="template" value="<?php echo html($template) ?>" />
