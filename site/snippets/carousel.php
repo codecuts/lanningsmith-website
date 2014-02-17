6b98cb7f75fae452cd70644e28f3535c10ab3ad0
Ethan Miller <ethanzanemiller@gmail.com>
2014-02-17 08:29:21 +0100

    Default.php modified to use new carousel.php snippet.

---
+++ site/snippets/carousel.php
@@ -0,0 +1,50 @@
+<?php if ($context == 'gridnav') : ?>
+	<div class='jcarousel'>
+		
+		<ul>
+			
+			<?php $i=0;?>	
+			<?php $gridcount = 12; ?>
+		
+			<?php foreach ($found->visible() as $item) : ?>
+		
+				<?php if ($i % $gridcount == 0) : ?>
+					<li>
+						<div>
+				<?php endif; ?>		
+				
+							<div style = "display : inline-block"> 
+								<img src="http://lorempixel.com/400/200" alt="dumdidum"/>
+								<div> 
+									<?php echo $item->title() ?>	
+								</div>	
+							</div>
+
+				<?php $i++;	?>
+		
+				<?php if ($i % $gridcount == 0) :?>
+						</div>
+					</li>
+				<?php endif;?>
+		
+			<?php endforeach; ?>
+
+		</ul>
+
+		<a class="jcarousel-control-prev" href="#">Prev</a>
+	    <a class="jcarousel-control-next" href="#">Next</a>
+	</div>
+<?php elseif ($content == 'mainframe') : ?>
+	<div class='jcarousel'>
+		<ul>
+			<?php foreach ($found->visible() as $item) : ?>
+				<li>
+					<img src="http://lorempixel.com/800/540/"/ alt="first image">
+					<!-- <div class="jcarousel-caption"><?php //echo $item->title(); ?></div>-->
+				</li>
+			<?php endforeach; ?>
+		</ul>
+		<a class="jcarousel-control-prev" href="#">Prev</a>
+	    <a class="jcarousel-control-next" href="#">Next</a>
+	</div>
+<?php endif; ?>
--- site/templates/default.php
+++ site/templates/default.php
@@ -2,28 +2,22 @@
 <?php //snippet('menu') ?>
 <?php //snippet('submenu') ?>
 
-<section class="content">
-	<div class="view-frame">
-		<div>
-
-
-		<?php
-
-			$category = $site->uri()->path()->last();
-			if($category == 'projects'){
-				$category = '';
-			}
-			echo $category;echo '<br>';
-			$projects = $pages->find('projects')->children();
-
-			$found = $projects->filterBy('categories', $category);
-			foreach($found as $p){
-				echo '<br>';
-				echo $p->title(); 
-			}
+<?php
+	$category = $site->uri()->path()->last();
+	if($category == 'projects'){
+		$category = '';
+	}
+	$projects = $pages->find('projects')->children();
+	$found = $projects->filterBy('categories', $category);
 ?>
 
-</div>
+<section class="gridnav">
+	<?php snippet('carousel', array('found' => $found, 'context'=>'gridnav')) ?>
+</section>
+
+<section class="main">
+	<div class="main-frame">
+		<?php //snippet('carousel', array('found' => $found, 'context'=>'main-frame')) ?>
 	</div>
 </section>
 
