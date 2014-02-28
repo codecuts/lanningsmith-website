62350c93b9772e13340a31f8a391f703d5c1108d
Maximilian Marcoll <quince@maximilianmarcoll.de>
2014-02-28 15:19:52 +0100

    backend project tags and hints

--- site/panel/blueprints/project.php
+++ site/panel/blueprints/project.php
@@ -7,17 +7,17 @@ pages: true
 files: false
 fields:
   title: 
-    label: Project Title
+    label: Project Title <br><font size= "1"> The Name of the Project (REQUIRED)</font>
     type:  text
   text: 
-    label: Project Description
+    label: Project Description <br><font size= "1"> Full description of the Project </font>
     type:  textarea
     size:  medium
   tags:
-   	label: Project Tags
-   	type: text
-  categories:
-    label: Categories
+   	label: Project Tags <br><font size= "1"> Enter tags for the Project here. Each tag should be one single word </font>
+   	type: tags
+  categories: 
+    label: Categories <br><font size= "1"> Choose what Categories the Project should belong to </font>
     type: multicheckbox
     options: 
       everybodys: Everybody's
