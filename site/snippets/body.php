b261b6bc496d544093ff7a815218f5f233fe6d37
Ethan Miller <ethanzanemiller@gmail.com>
2014-03-01 17:53:27 +0100

    using background property to set up/down arrows again. no ghosting issue on the up/down arrows.

--- site/snippets/body.php
+++ site/snippets/body.php
@@ -14,8 +14,8 @@
 			<div class="ctrl shutter" title="Open Thumb Menu"></div>
 			<div class="ctrl left" title="Previous Image"><img src="/assets/images/arrow_rightleft.png" alt"left arrow"/></div>
 			<div class="ctrl right" title="Next Image"><img src="/assets/images/arrow_rightleft.png" alt"right arrow"/></div>
-			<div class="ctrl up" title="Previous Project"><img src="/assets/images/arrow_updown.png" alt"up arrow"/></div>
-			<div class="ctrl down" title="Next Project"><img src="/assets/images/arrow_updown.png" alt"down arrow"/></div>
+			<div class="ctrl up" title="Previous Project"><!--<img src="/assets/images/arrow_updown.png" alt"up arrow"/>--></div>
+			<div class="ctrl down" title="Next Project"><!--<img src="/assets/images/arrow_updown.png" alt"down arrow"/>--></div>
 		</nav>
 		<div class="main-frame">
 			<?php //snippet('carousel', array('found' => $found, 'context'=>'mainframe')) ?>
