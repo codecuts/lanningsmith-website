794a6df75621af62e87977a1abaf5b684af99fc2
Ethan Miller <ethanzanemiller@gmail.com>
2014-03-14 17:46:12 +0100

    added metatags snippet for dealing with metatag data for facebook and twitter

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -21,29 +21,7 @@
     <meta name="robots" content="index, follow" />
 
     <!-- Facebook Metatags -->
-    <meta property="og:type"            content="website" />
-    <meta property="og:site_name"       content="<?php echo $site->title() ?>"/> 
-    <meta property="og:url"             content="<?php echo $site->url().'/'.$site->uri()->path(2); ?>" /> 
-    <?php if ($page->isHomePage() ) : ?>
-        <meta property="og:title" content="<?php echo $site->title() ?> - <?php echo $page->title()?>" />
-        <meta property="og:description" content="<?php echo $site->description()?>" />
-        <meta property="og:image" content="" />
-    <?php elseif ( isset($category_name) ) : ?>
-        <meta property="og:title" content="<?php echo $site->title.' - Category Archive - '.$category_name ?>" />
-        <meta property="og:description" content="" />
-        <meta property="og:image" content="" />
-    <?php elseif ( isset($project_name) ) : ?>
-        <meta property="og:title" content="<?php echo $site->title.' - '.$page->title()?>" />
-        <meta property="og:description" content="<?php echo $page->text() ?>" />
-        <meta property="og:image" content="" /> 
-        <?php if ( $pages->active()->template() == 'project' || $pages->active()->isHomePage() ) : ?>
-            <?php if ( $pages->active()->children()->first()->template() === 'image' ) : ?>
-            <meta property="og:image" content="<?php echo $pages->active()->children()->first()->images()->first()->url(); ?>" />
-            <?php elseif ( $pages->active()->children()->first()->template() === 'video' ) : ?>
-            <meta property="og:image" content="<?php echo videos::thumb($pages->active()->children()->first()->video_url()); ?>" />
-            <?php endif; ?>
-        <?php endif; ?>
-    <?php endif; ?>
+    <?php snippet('metatags',array('type' => 'facebook')); ?>
     
     <link rel="icon" type="image/png" href="<?php echo url('favicon.ico') ?>">
     <link rel="alternate" type="application/rss+xml" href="<?php echo url('blog/feed') ?>" title="Blog Feed" />
