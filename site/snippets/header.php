b0c36bdc3f517f61753d987241354404908a8a2a
Ethan Miller <ethanzanemiller@gmail.com>
2014-03-15 15:57:30 +0100

    added code to output correct description and keyword elements in header

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -15,10 +15,20 @@
 
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="description" content="<?php echo html($site->description()) ?>" />
     <!-- will need to get right for desktop version of this site: <meta name="viewport" content="width=device-width, initial-scale=1">-->
-    <meta name="keywords" content="<?php echo html($site->keywords()) ?>" />
     <meta name="robots" content="index, follow" />
+
+    <?php if($page->text() !== ''): ?>
+    <meta name="description" content="<?php echo html($page->text()) ?>" />
+    <?php else: ?>
+    <meta name="description" content="<?php echo html($site->description()) ?>" />
+    <?php endif ?>
+
+    <?php if($page->tags() !== ''): ?>
+    <meta name="keywords" content="<?php echo html($page->tags()) ?>" />
+    <?php else: ?>
+    <meta name="keywords" content="<?php echo html($site->keywords()) ?>" />
+    <?php endif ?>
 
     <!-- Facebook Metatags -->
     <?php snippet('metatags',array('type' => 'facebook')); ?>
