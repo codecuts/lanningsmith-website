51478fa821c61e601c97c0ac0563e46ef3a29178
Ethan Miller <ethanzanemiller@gmail.com>
2014-03-19 18:20:09 +0100

    cleaning up a bit

--- assets/app/ourCarousel.js
+++ assets/app/ourCarousel.js
@@ -2,30 +2,6 @@ define(["jquery", "jcarousel", "jcarousel-control"], function() {
 
 	var init = function() {
 
-		// initizialize the gridnav carousel
-		/*$('.jcarousel.thumbs')
-			.on('jcarousel:create jcarousel:reload', function() {
-		        var element = $(this),
-		            width = element.innerWidth(),
-		            height = element.innerHeight();
-
-	        	//element.jcarousel('items').css('width', width + 'px');
-		        //element.jcarousel('items').css('margin-right', '150px');
-
-				gi = element.find('.grid-item, .grid-item-rightmost' );
-
-				aspectRatio = 3/2;
-				itemWidth = (width-63)/4;
-				itemHeight = itemWidth*(1/aspectRatio);
-				gi.each(function() {
-		        	$(this).css('width', itemWidth);
-					$(this).css('height', itemHeight);
-		        });
-	            
-		    }).jcarousel({
-		        // Your configurations options
-		});*/
-
 		// initialize the projects carousel in the main-frame
 		$('.jcarousel.projects').on('jcarousel:create jcarousel:reload', function() {
 			// do some setup stuff...
@@ -48,34 +24,6 @@ define(["jquery", "jcarousel", "jcarousel-control"], function() {
 			carousel.find('a[class*="jcarousel-control-"]').vAlignInViewport();
 
 		}).jcarousel();
-
-		/*$('.jcarousel-control-prev').jcarouselControl({
-		    target: '-=1'
-		});
-
-		$('.jcarousel-control-next').jcarouselControl({
-		    target: '+=1'
-		});*/
-
-		/*$('.main .main-nav div').each(function() {
-			var ctrl = $(this),
-				elemClass = ctrl.attr('class'),
-				projectsCarousel = $('.jcarousel.projects'),
-				activeProjectCarousel = projectsCarousel.find('.jcarousel.project');
-
-			if ( elemClass == 'top' || elemClass == 'bottom' ) {
-				ctrl.jcarouselControl({ 
-					carousel: projectsCarousel, 
-					target: ( elemClass == 'top' ) ? '-=1' : '+=1'
-				});
-			}
-			else {
-				ctrl.jcarouselControl({
-					carousel: activeProjectCarousel,
-					target: ( elemClass == 'left' ) ? '-=1' : '+=1'
-				});
-			}
-		});*/
 
 	};
 
