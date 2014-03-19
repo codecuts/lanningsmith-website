a6137bedd403bcc66d090245e78fa3fd3febf498
Ethan Miller <ethanzanemiller@gmail.com>
2014-03-19 21:03:12 +0100

    changes to facebook metatags: when a url is linked to specific project in category, data that is presented to fb is projet data

--- site/snippets/metatags.php
+++ site/snippets/metatags.php
@@ -17,15 +17,27 @@ global $project_name;
 	    <meta property="og:image:url" content="<?php echo $pages->find("/home")->images()->first()->url() ?>" /> 
 		<?php endif; ?>
 	<?php elseif ( $project_name !== null ) : ?>
-	    <meta property="og:title" content="<?php echo $site->title.' - '.$page->title()?>" />
-	    <meta property="og:description" content="<?php echo html($page->text()) ?>" />
-        <?php if ( $pages->active()->children()->first()->template() === 'image' ) : ?>
-        <meta property="og:image" content="<?php echo $pages->active()->children()->first()->images()->first()->url(); ?>" />
-        <meta property="og:image:url" content="<?php echo $pages->active()->children()->first()->images()->first()->url(); ?>" />
-        <?php elseif ( $pages->active()->children()->first()->template() === 'video' ) : ?>
-        <meta property="og:image" content="<?php echo videos::thumb($pages->active()->children()->first()->video_url()); ?>" />
-        <meta property="og:image:url" content="<?php echo videos::thumb($pages->active()->children()->first()->video_url()); ?>" />
-        <?php endif; ?>
+		<?php if ( $category_name !== null ) : ?>
+			<meta property="og:title" content="<?php echo $site->title.' - '.ucwords($project_name) ?>" />
+		    <meta property="og:description" content="<?php echo html($pages->find('/projects/'.strtolower($project_name))->text()) ?>" />
+	        <?php if ( $pages->find('/projects/'.strtolower($project_name))->children()->first()->template() === 'image' ) : ?>
+	        <meta property="og:image" content="<?php echo $pages->find('/projects/'.strtolower($project_name))->children()->first()->images()->first()->url(); ?>" />
+	        <meta property="og:image:url" content="<?php echo $pages->find('/projects/'.strtolower($project_name))->children()->first()->images()->first()->url(); ?>" />
+	        <?php elseif ( $pages->find('/projects/'.strtolower($project_name))->children()->first()->template() === 'video' ) : ?>
+	        <meta property="og:image" content="<?php echo videos::thumb($pages->find('/projects/'.strtolower($project_name))->children()->first()->video_url()); ?>" />
+	        <meta property="og:image:url" content="<?php echo videos::thumb($pages->find('/projects/'.strtolower($project_name))->children()->first()->video_url()); ?>" />
+	        <?php endif; ?>
+		<?php else : ?>
+		    <meta property="og:title" content="<?php echo $site->title.' - '.$page->title()?>" />
+		    <meta property="og:description" content="<?php echo html($page->text()) ?>" />
+	        <?php if ( $pages->active()->children()->first()->template() === 'image' ) : ?>
+	        <meta property="og:image" content="<?php echo $pages->active()->children()->first()->images()->first()->url(); ?>" />
+	        <meta property="og:image:url" content="<?php echo $pages->active()->children()->first()->images()->first()->url(); ?>" />
+	        <?php elseif ( $pages->active()->children()->first()->template() === 'video' ) : ?>
+	        <meta property="og:image" content="<?php echo videos::thumb($pages->active()->children()->first()->video_url()); ?>" />
+	        <meta property="og:image:url" content="<?php echo videos::thumb($pages->active()->children()->first()->video_url()); ?>" />
+	        <?php endif; ?>
+	 	<?php endif; ?>
     <?php elseif ( $category_name !== null ) : ?>
 	    <meta property="og:title" content="<?php echo $site->title.' - Category Archive - '.$category_name ?>" />
 	    <meta property="og:description" content="An archive of projects in the category: <?php echo $category_name ?>" />
