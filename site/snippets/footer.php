6eb5c568e52df13ed5a0b4df3e8fceeaa6497424
Ethan Miller <ethanzanemiller@gmail.com>
2014-03-25 09:57:43 +0100

    made the footer script reference to js files relative

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -106,7 +106,7 @@
 	</script>
 
 	<!-- RequireJS: Loads jQuery and Other Modules -->
-	<script data-main="<?php echo $site->url() ?>/assets/js/app" src="<?php echo $site->url() ?>/assets/js/vendor/require.js"></script>
+	<script data-main="assets/js/app" src="assets/js/vendor/require.js"></script>
 
 	<!-- Google Analytics -->
 	<script>
